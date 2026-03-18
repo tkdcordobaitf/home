888b21411c4669f19a379f323c706fc7b50362fa
Agus Edme <edmealiciardi@gmail.com>
2026-03-18 20:23:35 -0300

    Version 0.2 - Dojanes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let player;
 
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: "315",
+    width: "560",
+    videoId: "weiDnCTeflM",
+  });
+}
+
+function goTo(seconds) {
+  if (!player) return;
+  player.seekTo(seconds, true);
+  player.pauseVideo();
+}
